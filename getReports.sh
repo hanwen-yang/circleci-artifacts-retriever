31a1be5039124b110e93c672a6ac0ddfe097b4fc
hanwen_yang <hanwen.yang@mheducation.com>
2019-07-10 23:59:35 -0400

    only retrieve file

--- getReports.sh
+++ getReports.sh
@@ -8,7 +8,7 @@ CIRCLE_TOKEN='YourCircleCIToken'
 
 curl $DOMAIN/api/v1.1/project/$VCS/$USERNAME/$PROJECT/$1/artifacts?circle-token=$CIRCLE_TOKEN | grep -o 'https://[^"]*' > artifacts.txt
 
-<artifacts.txt xargs -P4 -I % wget %?circle-token=$CIRCLE_TOKEN
+<artifacts.txt xargs -P4 -I % wget -O ${%##*/} %?circle-token=$CIRCLE_TOKEN
 
 find . -type f -name '*?circle-token=$CIRCLE_TOKEN' | while read f; do mv "$f" "${f%?circle-token=$CIRCLE_TOKEN}"; done
 
