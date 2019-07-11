3c294ba8affc2da55ad1b2f9e8004d592d5df717
hanwen_yang <hanwen.yang@mheducation.com>
2019-07-11 19:01:14 -0400

    create a separate function to get file

--- getReports.sh
+++ getReports.sh
@@ -6,10 +6,17 @@ USERNAME=YourUserName
 PROJECT=YourProjectName
 CIRCLE_TOKEN='YourCircleCIToken'
 
+function mywget()
+{
+     wget -O ${2##*/} "$2?circle-token=$1"
+}
+
+export -f mywget
+
 curl $DOMAIN/api/v1.1/project/$VCS/$USERNAME/$PROJECT/$1/artifacts?circle-token=$CIRCLE_TOKEN | grep -o 'https://[^"]*' > artifacts.txt
 
-<artifacts.txt xargs -P4 -I % wget -O ${%##*/} %?circle-token=$CIRCLE_TOKEN
+<artifacts.txt xargs -P4 -I {} bash -c 'mywget $CIRCLE_TOKEN "{}"'
 
-find . -type f -name '*?circle-token=$CIRCLE_TOKEN' | while read f; do mv "$f" "${f%?circle-token=$CIRCLE_TOKEN}"; done
+find . -type f -name "*?circle-token=$CIRCLE_TOKEN" | while read f; do mv "$f" "${f%?circle-token=$CIRCLE_TOKEN}"; done
 
 rm artifacts.txt
